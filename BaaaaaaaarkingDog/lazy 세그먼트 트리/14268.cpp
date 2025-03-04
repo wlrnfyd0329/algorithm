db1f35c8bb7bceffc010718beed76b44bdd37420
YooHyeokJin <wlrnfyd0329@gmail.com>
2025-03-05 00:10:31 +0900

    2025-03-04_2

--- BaaaaaaaarkingDog/lazy 세그먼트 트리/14268.cpp
+++ BaaaaaaaarkingDog/lazy 세그먼트 트리/14268.cpp
@@ -4,9 +4,94 @@
 
 using namespace std;
 
+int n, m;
+int idx = 0;
+int st[100005];
+int en[100005];
+vector<int> son[100005];
 vector<int> tree;
+vector<int> lazy;
+
+void dfs(int depth) {
+    st[depth] = ++idx;
+    for(auto k : son[depth]) {
+        dfs(k);
+    }
+    en[depth] = idx;
+}
+
+void update_lazy(int node, int start, int end) {
+    if (lazy[node] != 0) {
+        tree[node] += lazy[node];
+
+        if (start != end) {
+            lazy[node * 2] += lazy[node];
+            lazy[node * 2 + 1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+    return;
+}
+
+void update_tree(int node, int start, int end, int left, int right, int diff) {
+    update_lazy(node, start, end);
+
+    if (start > right || end < left) return;
+    if (left <= start && end <= right) {
+        tree[node] += diff;
+        if (start != end) {
+            lazy[node * 2] += diff;
+            lazy[node * 2 + 1] += diff;
+        }
+        return;
+    }
+
+    int mid = (start + end) >> 1;
+    update_tree(node * 2, start, mid, left, right, diff);
+    update_tree(node * 2 + 1, mid + 1, end, left, right, diff);
+}
+
+
+
+void query(int node, int start, int end, int index) {
+    update_lazy(node, start, end);
+
+    if (index < start || index > end) return;   
+    if (start == end) {
+        cout << tree[node] << "\n";
+        return;
+    }
+
+    int mid = (start + end) >> 1;
+    query(node * 2, start, mid, index);
+    query(node * 2 + 1, mid + 1, end, index);
+}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    cin >> n >> m;
+    int h = ceil(log2(n));
+    tree = vector<int>(1 << (h + 1));
+    lazy = vector<int>(1 << (h + 1));
+    for(int i = 1; i <= n; i++) {
+        int num; 
+        cin >> num;
+        if (num == -1) continue;
+        son[num].push_back(i);
+    }
+
+    dfs(1);
+
+    for (int i = 0; i < m; i++) {
+        int cmd, person, w;
+        cin >> cmd >> person;
+        if (cmd == 1) {
+            cin >> w;
+            update_tree(1, 1, n, st[person], en[person], w);
+        }
+        else {
+            query(1, 1, n, st[person]);
+        }
+    }
 }
--- ssafy/알고리즘 수업 (java)/3월 1주차/유혁진_2112.java
+++ ssafy/알고리즘 수업 (java)/3월 1주차/유혁진_2112.java
@@ -2,69 +2,91 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static int T;
-	static int D;
-	static int W;
-	static int K;
-	static int[][] film;
-	static int[][] new_film;
-	static boolean[] comb;
-	static int ans;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int tc = 1; tc <= T; tc++) {
-			st = new StringTokenizer(br.readLine());
-			D = Integer.parseInt(st.nextToken());
-			W = Integer.parseInt(st.nextToken());
-			K = Integer.parseInt(st.nextToken());
-			film = new int[D][W];
-			new_film = new int[D][W];
-			comb = new boolean[D];
-			
-			for(int i = 0; i < D; i++) {
-				st = new StringTokenizer(br.readLine());
-				for(int j = 0; j < W; j++) {
-					film[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-		
-			System.out.println("#" + tc);
-		}
-	}
-	
-	public static void combination(int n, int cnt, int max_cnt) {
-		if (cnt == max_cnt) return;
-		
-		for(int i = n; i < D; i++) {
-			comb[i] = true;
-			combination(i + 1, cnt + 1, max_cnt);
-			comb[i] = false;
-		}
-	}
-	
-	public static void ink() {
-		
-	}
-	
-	public static boolean test() {
-		for(int j = 0; j < W; j++) {
-			int cnt = 1;
-			for(int i = 1; i < D; i++) {
-				if (new_film[i][j] == new_film[i - 1][j]) {
-					cnt++;
-				}
-				else {
-					cnt = 1;
-				}
-				if (cnt == K) break;
-			}
-			if (cnt < K) return false;
-		}
-		
-		return true;
-	}
+    static int T;
+    static int D;
+    static int W;
+    static int K;
+    static int[][] film;
+    static int[][] new_film;
+    static int[] comb;
+    static boolean isDone;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        T = Integer.parseInt(br.readLine());
+
+        for(int tc = 1; tc <= T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            D = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            film = new int[D][W];
+            new_film = new int[D][W];
+            comb = new int[D];
+            isDone = false;
+
+            for(int i = 0; i < D; i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j = 0; j < W; j++) {
+                    film[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int cnt;
+            for(cnt = 0; cnt <= K; cnt++) {
+                combination(0, 0, cnt);
+                if (isDone) break;
+            }
+
+            System.out.println("#" + tc + " " + cnt);
+        }
+    }
+
+    public static void combination(int n, int cnt, int max_cnt) {
+        if (cnt == max_cnt) {
+            for(int i = 0; i < D; i++) {
+                if (comb[i] != 0) {
+                    for(int j = 0; j < W; j++) {
+                        new_film[i][j] = comb[i] - 1;
+                    }
+                }
+                else {
+                    for(int j = 0; j < W; j++) {
+                        new_film[i][j] = film[i][j];
+                    }
+                }
+            }
+
+            isDone = isDone || test();
+            
+            return;
+        }
+
+        for(int i = n; i < D; i++) {
+            comb[i] = 1;
+            combination(i + 1, cnt + 1, max_cnt);
+            comb[i] = 2;
+            combination(i + 1, cnt + 1, max_cnt);
+            comb[i] = 0;
+        }
+    }
+
+    public static boolean test() {
+        for(int j = 0; j < W; j++) {
+            int cnt = 1;
+            for(int i = 1; i < D; i++) {
+                if (new_film[i][j] == new_film[i - 1][j]) {
+                    cnt++;
+                }
+                else {
+                    cnt = 1;
+                }
+                if (cnt == K) break;
+            }
+            if (cnt < K) return false;
+        }
+
+        return true;
+    }
 
 }
