83ed1821404815bfc92a96e4b1f6116a699597f2
yhj0329 <yhjmir@naver.com>
2025-03-12 17:58:14 +0900

    2025-03-12

---
+++ ssafy/B형 문제/상품권배분/solution.cpp
@@ -0,0 +1,15 @@
+void init(int N, int mId[], int mNum[]) {
+	return;
+}
+
+int add(int mId, int mNum, int mParent) {
+	return 0;
+}
+
+int remove(int mId) {
+	return 0;
+}
+
+int distribute(int K) {
+	return 0;
+}
