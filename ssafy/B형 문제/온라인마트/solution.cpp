0b36c0ce880e31bf1060194ab68eac62629131f0
YooHyeokJin <wlrnfyd0329@gmail.com>
2025-08-20 02:24:33 +0900

    2025-08-19_2

--- ssafy/B형 문제/온라인마트/solution.cpp
+++ ssafy/B형 문제/온라인마트/solution.cpp
@@ -2,6 +2,7 @@
 
 #include <unordered_map>
 #include <set>
+#include <vector>
 #include <algorithm>
 
 #define MAX_PRODUCT 50005
@@ -53,38 +54,73 @@ int sell(int mID, int mCategory, int mCompany, int mPrice)
 {
     int temp_price = sale[mCategory][mCompany].amount;
 
-    product[product_idx] = {mID, mCategory, mCompany, mPrice, false};
+    product[product_idx] = { mID, mCategory, mCompany,  mPrice + temp_price, false };
     product_table[mID] = product_idx++;
-    sale[mCategory][mCompany].product.insert({mPrice + temp_price, mID});
+    sale[mCategory][mCompany].product.insert({ mPrice + temp_price, mID });
     return ++sale[mCategory][mCompany].cnt;
 }
 
 // 5,000
 int closeSale(int mID)
 {
-    if (!product_table[mID] || product[product_table[mID]].isClosed)
-        return -1;
-    int temp_price = product[product_table[mID]].mPrice - sale[product[product_table[mID]].mCate][product[product_table[mID]].mCom].amount;
-    if (temp_price <= 0)
-    {
-        product[product_table[mID]].isClosed = true;
-        return -1;
-    }
-    return temp_price;
+    int idx = product_table[mID];
+    if (!idx || product[idx].isClosed) return -1;
+    product[idx].isClosed = true;
+    sale[product[idx].mCate][product[idx].mCom].cnt--;
+    return product[idx].mPrice - sale[product[idx].mCate][product[idx].mCom].amount;
 }
 
 // 10,000
+// 단순하게 생각하면 최대 50,000이고 10,000번 연산하기에 시간이 초과된다고 생각할 수 있지만
+// 결국 N이 생겼지만 하나하나 지우기 때문에 50,000이 최대이며 NlogN임을 알 수 있다.
 int discount(int mCategory, int mCompany, int mAmount)
 {
-    return -1;
+    Sale* cur_sale = &sale[mCategory][mCompany];
+    cur_sale->amount += mAmount;
+    for (auto iter = cur_sale->product.begin(); iter != cur_sale->product.end();)
+    {
+        if (iter->first - cur_sale->amount > 0) break;
+        if (!product[product_table[iter->second]].isClosed) {
+            product[product_table[iter->second]].isClosed = true;
+            cur_sale->cnt--;
+        }
+        iter = cur_sale->product.erase(iter);
+    }
+    return cur_sale->cnt;
 }
 
 // 1,000
 RESULT show(int mHow, int mCode)
 {
     RESULT res;
+    vector<pair<int, int>> pro;
 
-    res.cnt = -1;
+    for (int i = 1; i < MAX_CATE; i++) {
+        if (mHow == 1 && i != mCode) continue;
+        for (int j = 1; j < MAX_COMP; j++) {
+            if (mHow == 2 && j != mCode) continue;
+            int cnt = 0;
+            for (auto k : sale[i][j].product) {
+                if (cnt >= 5) break;
+                if (!product[product_table[k.second]].isClosed) {
+                    pro.push_back({ k.first - sale[i][j].amount, k.second });
+                    cnt++;
+                }
+            }
+        }
+    }
+
+    sort(pro.begin(), pro.end());
+    if (pro.size() <= 5) {
+        res.cnt = pro.size();
+    }
+    else {
+        res.cnt = 5;
+    }
+    
+    for (int i = 0; i < res.cnt; i++) {
+        res.IDs[i] = pro[i].second;
+    }
 
     return res;
 }
